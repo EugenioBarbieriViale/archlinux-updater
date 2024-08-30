f078a33c18c9e45a7b9a842c6fcdc0379e6a0b2e
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-08-30 10:37:59 +0200

    new

--- system-update.cpp
+++ system-update.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <cstdlib>
+#include <chrono>
 
 std::string get_date() {
     time_t rawtime;
@@ -15,6 +16,14 @@ std::string get_date() {
     strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
     std::string str(buffer);
     return buffer;
+}
+
+int get_day_of_week() {
+    struct tm *timeval;
+    time_t tt;
+    tt = time(NULL);
+    timeval = localtime(&tt);
+    return (timeval->tm_wday);
 }
 
 std::vector<std::string> get_array_of_lines() {
@@ -41,7 +50,8 @@ void update_ascii() {
 }
 
 int main() {
-    const int interval = 6;
+    // 0 -> Sunday ... 6 -> Saturday
+    const int WEEK_DAY_UPDATE = 0;
 
     std::ofstream file;
     file.open("database.log", std::ios::app);
@@ -49,15 +59,17 @@ int main() {
     std::string date = get_date();
 
     std::string sday = {date[0], date[1]};
-    int day = std::stoi(sday);
+    const int day = std::stoi(sday);
 
     std::vector<std::string> arr = get_array_of_lines();
     std::string last_date = arr[arr.size()-1];
 
     std::string slast_day = {last_date[0], last_date[1]};
-    int last_day = std::stoi(slast_day);
+    const int last_day = std::stoi(slast_day);
 
-    if (day != last_day) {
+    const int weekday = get_day_of_week();
+
+    if (weekday == WEEK_DAY_UPDATE && day != last_day) {
         file << date << '\n';
         update_ascii();
         std::system("sudo pacman -Syu && yay");
